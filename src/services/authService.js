4337e2bcb3c8ea32a1d60b50dc7b82b129eebdc8
Hrishabh Jain <hrishabh507@gmail.com>
2024-03-29 00:48:54 +0530

    feat: add admin services

--- src/services/authService.js
+++ src/services/authService.js
@@ -3,44 +3,69 @@ const jwt = require('jsonwebtoken');
 const moment = require('moment');
 const { StatusCodes } = require('http-status-codes');
 const User = require('../db/models/User');
+const Admin = require('../db/models/Admin');
 const ApiError = require('../errors/ApiError');
 const { getUserByEmail, getUserByRefreshToken } = require('./userService');
+const { getAdminByRefreshToken, getAdminByEmail } = require('./adminService');
 const envConfig = require('../config/env');
 const tokenService = require('./tokenService');
 const tokenTypes = require('../utils/constants/tokenTypes');
+const ROLES = require('../utils/constants/roles');
 
-const signup = async (userObj) => {
+const signup = async (userObj, role) => {
   try {
-    if (await User.isEmailTaken(userObj.email)) {
-      throw new ApiError(StatusCodes.BAD_REQUEST, 'Email already taken!');
-    }
     const hashedPassword = await bcrypt.hash(userObj.password, 12);
-    const user = await User.create({
-      email: userObj.email,
-      password: hashedPassword,
-      firstName: userObj.firstName,
-      lastName: userObj.lastName,
-    });
-    return user;
+    if (role === ROLES.USER) {
+      if (await User.isEmailTaken(userObj.email)) {
+        throw new ApiError(StatusCodes.BAD_REQUEST, 'Email already taken!');
+      }
+      const user = await User.create({
+        email: userObj.email,
+        password: hashedPassword,
+        firstName: userObj.firstName,
+        lastName: userObj.lastName,
+      });
+      return user;
+    } else if (role === ROLES.ADMIN) {
+      if (await Admin.isEmailTaken(userObj.email)) {
+        throw new ApiError(StatusCodes.BAD_REQUEST, 'Email already taken!');
+      }
+      const admin = await Admin.create({
+        email: userObj.email,
+        password: hashedPassword,
+        firstName: userObj.firstName,
+        lastName: userObj.lastName,
+      });
+      return admin;
+    }
   } catch (error) {
     throw new ApiError(StatusCodes.INTERNAL_SERVER_ERROR, error.message);
   }
 };
 
-const loginWithEmailAndPassword = async (email, password) => {
-  const user = await getUserByEmail(email);
+const loginWithEmailAndPassword = async (email, password, role) => {
+  let user;
+  if (role === ROLES.USER) {
+    user = await getUserByEmail(email);
+  } else if (role === ROLES.ADMIN) {
+    user = await getAdminByEmail(email);
+  }
   if (!user || !(await user.isPasswordMatch(password))) {
     throw new ApiError(StatusCodes.UNAUTHORIZED, 'Incorrect email or password');
   }
   return user;
 };
 
-const refresh = async (cookies) => {
+const refresh = async (cookies, role) => {
   if (!cookies?.jwt) throw new ApiError(StatusCodes.UNAUTHORIZED, 'No refresh token found in the cookies!');
   const refreshToken = cookies.jwt;
-  const user = await getUserByRefreshToken(refreshToken);
+  let user;
+  if (role === ROLES.USER) {
+    user = await getUserByRefreshToken(refreshToken);
+  } else if (role === ROLES.ADMIN) {
+    user = await getAdminByRefreshToken(refreshToken);
+  }
   if (!user) throw new ApiError(StatusCodes.UNAUTHORIZED, 'User not found!');
-
   let accessToken;
   jwt.verify(refreshToken, envConfig.jwt.secret, (err, decoded) => {
     if (err) {
