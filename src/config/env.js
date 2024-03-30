1c92a4ce8721ed59ce93faecb4d017ca956ac3e8
Hrishabh Jain <hrishabh507@gmail.com>
2024-03-30 22:32:27 +0530

    feat: setup redis cloud db

--- src/config/env.js
+++ src/config/env.js
@@ -4,6 +4,11 @@ const envConfig = {
   port: process.env.PORT || 3000,
   nodeEnv: process.env.NODE_ENV,
   mongoUri: process.env.MONGODB_URI,
+  redis: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    password: process.env.REDIS_PASSWORD,
+  },
   jwt: {
     secret: process.env.JWT_SECRET,
     accessExpirationMinutes: process.env.JWT_ACCESS_EXPIRATION_MINUTES,
